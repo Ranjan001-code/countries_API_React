b1667a8b2a0887ea3a22a43e41406486076338b2
Ranjan001 <mhtcet2022coeprs@gmail.com>
2026-02-03 19:28:55 +0530

    useEffect Hook implemented

--- App.jsx
+++ App.jsx
@@ -8,31 +8,18 @@ import "./App.css";
 
 const App = () => {
   const [query, setQuery] = React.useState("");
-  const [load, setLoad] = useState(true); //true for first render
+  // const [load, setLoad] = useState(true); //true for first render
   // console.log("rendering App from App.jsx");
   return (
     <>
-      {load == true ? (
-        <button
-          className="load"
-          onClick={() => {
-            setLoad(false);
-          }}
-        >
-          Load the Website
-        </button>
-      ) : (
-        <>
-          <Header />
-          <main>
-            <div className="search-filter-container">
-              <SearchBar setQuery={setQuery} />
-              <SelectMenu />
-            </div>
-            <CountriesList query={query} />
-          </main>
-        </>
-      )}
+      <Header />
+      <main>
+        <div className="search-filter-container">
+          <SearchBar setQuery={setQuery} />
+          <SelectMenu />
+        </div>
+        {query == "unmount" ?"":<CountriesList query={query} />}
+      </main>
     </>
   );
 };
--- components/CountriesList.jsx
+++ components/CountriesList.jsx
@@ -1,9 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import countriesData from "../countriesData";
 import CountryCard from "./CountryCard";
 
 export default function CountriesList({query}) {
-  
+   
+  // const [countriesData,setCountriesData] = useState([]);
+  useEffect(()=>{
+    // fetch('https://restcountries.com/v3.1/all')
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     console.log(data)
+    //     setCountriesData(data)
+    //   })
+
+    const time = new Date()
+   const setId = setInterval(()=>{
+      console.log("component is running");
+    },1000)
+
+
+
+      return ()=>{
+        console.log("unmounting")
+        console.log((new Date()-time)/1000 + " seconds component was mounted")
+        clearInterval(setId);
+      }
+  },[])
 
   return (
     <div className="countries-container">
