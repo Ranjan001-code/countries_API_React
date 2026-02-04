9d0a913fd1b7dfe4c0a4ceaf6e9bee000926fda2
Ranjan001 <mhtcet2022coeprs@gmail.com>
2026-02-04 23:11:21 +0530

    dynamic routing implemented

--- components/CountryCard.jsx
+++ components/CountryCard.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export default function CountryCard({ name, flag, population, region, capital }) {
   // console.log(name);
   return (
-    <a className="country-card" href={`/country?name=${name}`}>
+    <Link className="country-card" to={`/${name}`}>
       <img src={flag} alt={name + ' Flag'} />
       <div className="card-text">
         <h3 className="card-title">{name}</h3>
@@ -20,6 +20,6 @@ export default function CountryCard({ name, flag, population, region, capital })
           <b>Capital: </b>{capital}
         </p>
       </div>
-    </a>
+    </Link>
   )
 }
--- components/CountryDetail.jsx
+++ components/CountryDetail.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import "../countriesData.js";
 import countriesData from "../countriesData.js";
+import { useParams } from "react-router-dom";
 
-export default function CountryDetail(){
-  const countryName = new URLSearchParams(location.search).get("name");
-  let hello;
+export default function CountryDetail() {
+  // const countryName = new URLSearchParams(location.search).get("name");
+  const countryName = useParams().country;
+
+  let hello = undefined;
   countriesData.map((country) => {
     if (country.name.common === countryName) {
       hello = country.flags.svg;
     }
   });
+
   return (
     <>
-      <div>Welcome to {countryName}</div>
-      <img src={hello} style={{height:"200px"}}/>
+      <button
+        onClick={() => window.history.back()}
+        style={{ margin: "10px 20px", padding: "5px 10px" }}
+      >
+        Back
+      </button>
+      {hello === undefined ? (
+        <div>Country not found</div>
+      ) : (
+        <>
+          <div>Welcome to {countryName}</div>
+          <img src={hello} style={{ height: "200px", margin: "20px" }} />
+        </>
+      )}
     </>
   );
 }
